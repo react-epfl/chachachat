3e6a08b92c743468192f737f61c1291c9261dc55
Bruno Didot <bdidot@gmail.com>
2013-11-13 23:45:00 +0100

    configure express to listen on https and removed express sample code

--- chachachatsrv.js
+++ chachachatsrv.js
@@ -4,19 +4,14 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
+var https = require('https');
+var fs = require('fs');
 var path = require('path');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
@@ -29,9 +24,13 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+var credentials = {
+  key: fs.readFileSync('config/chachachat-key.pem'),
+  cert: fs.readFileSync('config/chachachat-cert.pem')
+};
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = https.createServer(credentials, app);
+
+server.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
