3ab45548209bb2518b4a692608f6a1eac0fd57e1
Andrii Vozniuk <andrii.vozniuk@epfl.ch>
2014-07-03 14:23:12 +0200

    update server start script

--- start_production.sh
+++ start_production.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-forever start --append -p /chachachat -l log/forever.log -o log/out.log -e log/err.log server.js
+forever start --append -p /chachachat -l log/forever.log -o log/out.log -e log/err.log /chachachat/server.js
