e92196b7960b9fca3c3550a4aa4c2c8fcd698c2a
Alex Wild <alex.j.wild@googlemail.com>
2015-05-06 11:21:50 +0200

    create launch daemon. Closes #60

--- start_production.sh
+++ start_production.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-forever start --append -p /chachachat -l log/forever.log -o log/out.log -e log/err.log /chachachat/server.js
+root="/chachachat"
+
+cd $root
+forever stop $root/server.js > /dev/null | exit 0
+forever start --append -p $root -l $root/log/forever.log -o $root/log/out.log -e $root/log/err.log $root/server.js
