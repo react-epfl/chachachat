316056052051204edb055745e0144a2d0fa7c182
Alex Wild <alex.j.wild@googlemail.com>
2015-09-10 14:14:53 +0200

    set working directory of forever. Closes #62

--- start_production.sh
+++ start_production.sh
@@ -4,4 +4,4 @@ root="/chachachat"
 
 cd $root
 forever stop $root/server.js > /dev/null | exit 0
-forever start --append -p $root -l $root/log/forever.log -o $root/log/out.log -e $root/log/err.log $root/server.js
+forever start --append --workingDir $root -p $root -l log/forever.log -o log/out.log -e log/err.log server.js
